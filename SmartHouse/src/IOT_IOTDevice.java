0cff9e6d8790bf78140d5af09ffdeed5d86830de
Gustavo <gustavoalvesbezerra@gmail.com>
2017-09-06 15:51:17 -0300

    Creates IOT_IOTDevice class: IOTDevice representation seen by the IOT Itself

---
+++ SmartHouse/src/IOT_IOTDevice.java
@@ -0,0 +1,14 @@
+//IOTDevice representation as seen by the IOT itself
+public class IOT_IOTDevice extends IOTDevice {
+	public void update() {
+		System.out.println("TODO: Implement update method");
+	}
+	
+	public void remove() {
+		System.out.println("TODO: Implement update method");
+	}
+	
+	public void discover() {
+		System.out.println("TODO: Implement discover method");
+	}
+}
