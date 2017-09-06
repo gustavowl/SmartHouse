807e9d0d194a6aa1e10e2cb6d7d354ff5822d0d1
Gustavo <gustavoalvesbezerra@gmail.com>
2017-09-06 15:45:14 -0300

    Creates abstract IOTDevice class

---
+++ SmartHouse/src/IOTDevice.java
@@ -0,0 +1,34 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+abstract public class IOTDevice {
+	protected InetAddress peerAddress;
+	protected int peerPort;
+	protected ArrayList<Method> methods;
+	
+	protected IOTDevice() {
+		this(null, 0);
+	}
+	
+	protected IOTDevice(InetAddress peerAddress, int peerPort) {
+		this.peerAddress = peerAddress;
+		this.peerPort = peerPort;
+		methods.add(new Method("update"));
+		methods.add(new Method("remove"));
+	}
+	
+	public InetAddress getPeerAddress() {
+		return peerAddress;
+	}
+	
+	public int getPeerPort() {
+		return peerPort;
+	}
+	
+	public abstract void update();
+	
+	public abstract void remove();
+	
+	public abstract void discover();
+	
+}
