e124d4d1134e6a66a994a0b835d1c405a2fe709c
Gustavo <gustavoalvesbezerra@gmail.com>
2017-09-06 15:49:30 -0300

    Creates AppIOTDevice class: IOTDevice representation seen by the App

---
+++ SmartHouse/src/AppIOTDevice.java
@@ -0,0 +1,15 @@
+//IOTDevice representation as seen by the App
+public class AppIOTDevice extends IOTDevice {
+	
+	public void update() {
+		System.out.println("TODO: Implement update method");
+	}
+	
+	public void remove() {
+		System.out.println("TODO: Implement update method");
+	}
+	
+	public void discover() {
+		System.out.println("TODO: Implement discover method");
+	}
+}
